9a9ebaded832d4da254c903249a37077c0a7ca9e
mdtanvir037 <hossainmdjabed037@gmail.com>
2021-10-19 17:50:20 +0600

    private route

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AuthProvider from "../../Context/AuthProvider";
 import AboutUs from "../Pages/AboutUs/AboutUs";
 import ContactUs from "../Pages/Contactus/ContactUs";
 import FAQ from "../Pages/FAQ/FAQ";
@@ -7,34 +8,37 @@ import Footer from "../Pages/Footer/Footer";
 import Header from "../Pages/Header/Header";
 import HomePage from "../Pages/HomePage/HomePage";
 import Login from "../Pages/Login/Login";
+import PrivateRoute from "../Pages/Private/PrivateRoute";
 
 const Home = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/">
-            <HomePage></HomePage>
-          </Route>
-          <Route path="/home_page">
-            <HomePage></HomePage>
-          </Route>
-          <Route path="/about-us">
-            <AboutUs></AboutUs>
-          </Route>
-          <Route path="/contact_us">
-            <ContactUs></ContactUs>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-          <Route path="/faq">
-            <FAQ></FAQ>
-          </Route>
-        </Switch>
-        <Footer></Footer>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <HomePage></HomePage>
+            </Route>
+            <Route path="/home_page">
+              <HomePage></HomePage>
+            </Route>
+            <PrivateRoute path="/about-us">
+              <AboutUs></AboutUs>
+            </PrivateRoute>
+            <Route path="/contact_us">
+              <ContactUs></ContactUs>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <PrivateRoute path="/faq">
+              <FAQ></FAQ>
+            </PrivateRoute>
+          </Switch>
+          <Footer></Footer>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 };
---
+++ src/Components/Pages/Private/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import useAuth from "../../../Hooks/useAuth";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user } = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user.displayName ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
