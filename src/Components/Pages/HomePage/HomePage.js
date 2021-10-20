019f308a6514d939d0230ca0a88f32adf2fde48e
mdtanvir037 <hossainmdjabed037@gmail.com>
2021-10-20 14:50:44 +0600

    consumer staisfiction

--- src/Components/Pages/HomePage/HomePage.js
+++ src/Components/Pages/HomePage/HomePage.js
@@ -180,6 +180,161 @@ const HomePage = () => {
       <div className="afterbanner py-4">
         <Services></Services>
       </div>
+      <div className="my-5 container">
+        <div className="row py-5 alert alert-success shadow-lg ">
+          <div className="col-md-5 col-lg-5 col-12">
+            <img
+              src="https://milvikbd.com/_next/image?url=%2FCovid-Care-Banner.png&w=256&q=75"
+              alt=""
+            />
+          </div>
+          <div className="col-md-7 col-lg-7 col-12 text-start">
+            <h1>
+              Covid-19 Care By <span className="fw-bold">Click Medic</span>
+            </h1>
+            <p>
+              All of our products cover covid. Our CovidCare package also
+              provides you or your family with dedicated support from MILVIK’s
+              specialist doctors every two days.
+            </p>
+            <button className="btn btn-outline-success shadow fw-bold">
+              View details <i className="ms-2 fas fa-long-arrow-alt-right"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="my-5 container">
+        <div className="row">
+          <div className="col-md-4 col-sm-12 col-lg-4">
+            <h1 className="fw-bold text-start mt-5 mb-3">
+              Why we are your <span className="primarycolor ">best choice</span>
+            </h1>
+            <p className="text-start mb-5" style={{ lineHeight: "30px" }}>
+              Over the last 9 years, we have served more than 4 million
+              customers in Bangladesh, providing access to primary healthcare
+              and risk protection in their times of need.
+            </p>
+            <div className="row mt-3">
+              <div className="col-3" style={{ marginRight: "-26px" }}>
+                <img
+                  src="https://milvikbd.com/_next/image?url=%2FBlue.svg&w=1920&q=75"
+                  alt=""
+                />
+              </div>
+              <div className="col-9 text-start">
+                <h5 className="fw-bold">Affordable plans</h5>
+                <p>Starting from just 145 Tk monthly</p>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-3" style={{ marginRight: "-26px" }}>
+                <img
+                  src="https://milvikbd.com/_next/image?url=%2FGreen.svg&w=1920&q=75"
+                  alt=""
+                />
+              </div>
+              <div className="col-9 text-start">
+                <h5 className="fw-bold">Limited waiting</h5>
+                <p>Maximum protection on what you are paying for</p>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-3" style={{ marginRight: "-26px" }}>
+                <img
+                  src="https://milvikbd.com/_next/image?url=%2FRed.svg&w=1920&q=75"
+                  alt=""
+                />
+              </div>
+              <div className="col-9 text-start">
+                <h5 className="fw-bold">Simple & easy</h5>
+                <p> Digital registration & claims payment.</p>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-3" style={{ marginRight: "-26px" }}>
+                <img
+                  src="https://milvikbd.com/_next/image?url=%2FYellow.svg&w=1920&q=75"
+                  alt=""
+                />
+              </div>
+              <div className="col-9 text-start">
+                <h5 className="fw-bold">Paperless & no medical Hassle-free </h5>
+                <p>registration without any paperwork</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-8 col-sm-12 col-lg-8">
+            <div className="row">
+              <div>
+                <h1 className="fw-bold text-start mt-5 mb-3">
+                  <span className="primarycolor ">
+                    What users say about their experience
+                  </span>
+                </h1>
+                <p className="text-start mb-5" style={{ lineHeight: "30px" }}>
+                  Trust being the biggest foundation of our service, we try to
+                  ensure users have a positive experience with all of our
+                  service dimensions.
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4 col-lg-4 col-sm-12 mb-2">
+                <div className="card h-100 alert alert-success">
+                  <div className="card-body">
+                    <img
+                      className="rounded-circle"
+                      src="https://milvikbd.com/_next/image?url=%2FCustomer-1.png&w=64&q=75"
+                      alt=""
+                    />
+                    <div className="card-title fw-bold mt-4">Ishak Hossain</div>
+                    <p className="mt-2">
+                      The doctor's treatment was excellent. They gave me enough
+                      time and helped me recover from home.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 col-lg-4 col-sm-12 mb-2">
+                <div className="card h-100 alert alert-success">
+                  <div className="card-body">
+                    <img
+                      className="rounded-circle"
+                      src="https://milvikbd.com/_next/image?url=%2FCustomer-2.png&w=64&q=75"
+                      alt=""
+                    />
+                    <div className="card-title fw-bold mt-4">
+                      Abdullah Al Mamun
+                    </div>
+                    <p className="mt-2">
+                      Getting Milvik Health was one of the best decisions I
+                      took. Milvik doctors helped me during a lot of emergency
+                      health situations. I also got super fast claims money from
+                      Milvik when I was admitted to the hospital.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 col-lg-4 col-sm-12 mb-2">
+                <div className="card h-100 alert alert-success">
+                  <div className="card-body">
+                    <img
+                      className="rounded-circle"
+                      src="https://milvikbd.com/_next/image?url=%2FCustomer-3.png&w=64&q=75"
+                      alt=""
+                    />
+                    <div className="card-title fw-bold mt-4">Masud Rana</div>
+                    <p className="mt-2">
+                      The doctor's treatment was excellent. They gave me enough
+                      time and helped me recover from home.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
